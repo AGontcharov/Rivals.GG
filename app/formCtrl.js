9957df0f6833cad2554225c313c60a6719ee89bd
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-08-08 01:10:56 -0400

    Stored records in array

--- app/formCtrl.js
+++ app/formCtrl.js
@@ -3,6 +3,7 @@ angular
 	.controller('formCtrl', ['$scope', '$http', function($scope, $http) {
 		var requestURL;
 		var summonerId;
+		var records = [];
 
 		$scope.search = function() {
 
@@ -10,39 +11,45 @@ angular
 			var res = string.split(',');
 			console.log(res);
 
-			requestURL = $scope.region + '.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + $scope.name
-			console.log(requestURL);
+			for (s of res) {
+				// requestURL = $scope.region + '.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + $scope.name
+				requestURL = $scope.region + '.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + s;
+				console.log(requestURL);
 
-			$http({
-				method: 'GET',
-				url: 'http://127.0.0.1:5000',
-				params: { "url": requestURL }
-			}).then(function successCallback(response) {
-				// This callback will be called asynchronously when the response is available			
-				console.log(response.status);
-				console.log(response.data);
+				$http({
+					method: 'GET',
+					url: 'http://127.0.0.1:5000',
+					params: { "url": requestURL }
+				}).then(function successCallback(response) {
+					console.log(response.status);
+					// console.log(response.data);
 
-				summonerId = response.data.id;
-				console.log(summonerId);
-				console.log(response.data.profileiconid);
+					summonerId = response.data.id;
+					console.log(summonerId);
 
-				$scope.profileIcon = response.data.profileIconId;
-				$scope.summonerName = response.data.name;
-				$scope.summonerLevel = response.data.summonerLevel;
-				$scope.lastActivity = response.data.revisionDate;
-				ranked(summonerId);
-				$scope.result = 'found';
+					var entry =
+					{
+						profileIcon : response.data.profileIconId,
+						summonerName : response.data.name,
+						summonerLevel : response.data.summonerLevel,
+						lastActivity : response.data.revisionDate
+					}
+					ranked(summonerId, entry);
+					records.push(entry);
+					$scope.result = 'found';
 
-			}, function errorCallback(response) {
-				// Called asynchronously if an error occurs or server returns response with an error status.
-				console.log(response.status);
-				console.log(response.data);
-				$scope.result = 'missing';
+				}, function errorCallback(response) {
+					console.log(response.status);
+					console.log(response.data);
+					$scope.result = 'missing';
 
-			});
+				});
+			}
+			console.log(records);
+			$scope.records = records;
 		}
 
-		function ranked(id) {
+		function ranked(id, obj) {
 			requestURL = $scope.region + '.api.riotgames.com/lol/league/v3/positions/by-summoner/' + id;
 
 			$http({
@@ -51,18 +58,16 @@ angular
 				params: { "url": requestURL }
 			}).then(function successCallback(response) {
 				console.log('Queue:', response.status);
-				console.log(response.data);
+				// console.log(response.data);
 
 				if (0 < response.data.length) {
-
-					$scope.soloTierIcon = '/tier-icons/' + response.data[0].tier + '_' + response.data[0].rank;
-
-					$scope.soloLeagueName = response.data[0].leagueName;
-					$scope.soloTier = response.data[0].tier;
-					$scope.soloDivision = response.data[0].rank;
-					$scope.soloLP = response.data[0].leaguePoints;
-					$scope.soloWins = response.data[0].wins;
-					$scope.soloLosses = response.data[0].losses;
+					obj.soloIcon = '/tier-icons/' + response.data[0].tier + '_' + response.data[0].rank;
+					obj.soloLeagueName = response.data[0].leagueName;
+					obj.soloTier = response.data[0].tier;
+					obj.soloDivision = response.data[0].rank;
+					obj.soloLP = response.data[0].leaguePoints;
+					obj.soloWins = response.data[0].wins;
+					obj.soloLosses = response.data[0].losses;
 					$scope.soloActive = 'true';
 				}
 				else {
@@ -71,14 +76,13 @@ angular
 				}
 
 				if (1 < response.data.length) {
-					$scope.flexTierIcon = '/tier-icons/' + response.data[1].tier + '_' + response.data[1].rank;
-
-					$scope.flexLeagueName = response.data[1].leagueName;
-					$scope.flexTier = response.data[1].tier;
-					$scope.flexDivision = response.data[1].rank;
-					$scope.flexLP = response.data[1].leaguePoints;
-					$scope.flexWins = response.data[1].wins;
-					$scope.flexLosses = response.data[1].losses;
+					obj.flexIcon = '/tier-icons/' + response.data[1].tier + '_' + response.data[1].rank;
+					obj.flexLeagueName = response.data[1].leagueName;
+					obj.flexTier = response.data[1].tier;
+					obj.flexDivision = response.data[1].rank;
+					obj.flexLP = response.data[1].leaguePoints;
+					obj.flexWins = response.data[1].wins;
+					obj.flexLosses = response.data[1].losses;
 					$scope.flexActive = 'true';
 				}
 				else {
