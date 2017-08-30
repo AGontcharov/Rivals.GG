61e5321c10509cb6e2b308ba80e39d0bdbbf5c69
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-08-30 19:28:12 -0400

    Pushing to GitHub

--- database/database.js
+++ database/database.js
@@ -4,8 +4,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: '127.0.0.1',
 	user: 'rivals.gg',
-	password: 'E33e70yop',
-	database: 'rivals'
+	password: '',
+	database: ''
 });
 
 // Connect to the database
