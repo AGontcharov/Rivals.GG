624dae5853dcd800d00060a84374ac5521040fc7
Alexander Gontcharov <gontchaa@mail.uoguelph.ca>
2017-09-10 15:13:54 -0400

    Forgot to add file

---
+++ public/directives/compareTo.directive.js
@@ -0,0 +1,28 @@
+angular
+	.module('myApp')
+	.directive('compareTo', function() {
+
+		return {
+			require: 'ngModel',
+			scope: { 
+				otherModelValue: '=compareTo' 
+			},
+			link: function(scope, element, attr, ctrl) {
+
+				// False sets the $error property - what kind of backwards bullshit logic is this
+				ctrl.$validators.compareTo = function(modelValue) {
+
+					// Set $error.compareTo
+					if (modelValue) return modelValue === scope.otherModelValue;
+
+					// Prevent appending to $error.required message
+					else return true;
+				}
+
+				// Needed to watch password input incase confirm password is typed first
+				scope.$watch('otherModelValue', function() {
+					ctrl.$validate();							
+				});
+			}
+		}
+});
